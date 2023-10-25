4ae7cbdf63e59556d5d8391458e577f91a6388c7
MattiaBottoni <105924586+MattiaBottoni@users.noreply.github.com>
2023-10-25 09:53:50 +0200

    Add files via upload

--- Lean 4 Exercise Sheets/Level 3.lean
+++ Lean 4 Exercise Sheets/Level 3.lean
@@ -2,7 +2,7 @@ import Mathlib.Tactic
 import Mathlib.Data.Real.Basic
 
 variable (n : ℕ)
---variable (x y : ℝ)
+variable (x y : ℝ)
 
 open Nat
 open BigOperators
@@ -27,41 +27,60 @@ rw [Nat.succ_add]
 rw [hd]
 done
 
-
-example: ∑ k in range (n+1), k = (n*(n+1))/2 := by
+example : ∑ k in range (n+1), k = n*(n+1)/(2 : ℕ) := by
 induction' n with d hd
-rw [Nat.zero_mul]
-sorry
-rw [succ_eq_add_one]
+rw [sum_range_one]
+simp
 rw [sum_range_succ]
 rw [hd]
-
+rw [succ_eq_add_one]
+ring_nf
+rw [add_comm]
+--rw [div_add'] --strange
+--rw [div_left_inj]
+sorry
 done
 
 
-example : ∑ k in Finset.Ico 1 n, 2*k-1 = n^2 := by
+example : ∑ k in Finset.Ico 1 (n+1), (2*k-1) = n^2 := by
 induction' n with d hd
 simp
 rw [succ_eq_add_one]
 rw [sum_Ico_succ_top]
-sorry
-sorry
+rw [add_comm]
+rw [hd]
+ring_nf
+rw [add_comm 2]
+rw [Nat.add_sub_assoc]
+rw [add_comm]
+rw [add_left_inj]
+rw [add_comm]
+rw [add_left_inj]
+simp
+linarith
+linarith
 done
 
 
 --Exercises from the exercise sheet 3---------------------------------------------------------
 
 --exercise 3.1
-example : ((Finset.range n).sum Nat.succ) ^ 2 = (Finset.range n).sum (λ k ↦ k^3) := by
+example : (∑ k in range (n+1), k)^2 = ∑ k in range (n+1), k^3 := by
 induction' n with d hd
-sorry
-sorry
-done
-
---exercise 3.1
-example : (∑ k in range n, k)^2 = ∑ k in range n, k^3 := by
-induction' n with d hd
-sorry
+rw [sum_range_one]
+rw [sum_range_one]
+simp
+rw [sum_range_succ]
+rw [add_sq]
+rw [hd]
+rw [succ_eq_add_one]
+conv_rhs => rw [sum_range_succ]
+rw [add_assoc]
+rw [add_left_cancel_iff]
+rw [sum_range_id] --Gauss summation formula!
+simp
+--rw [mul_div (d+1) d 2]
+--rw [← mul_assoc 2 (d + 1) d / 2]
 sorry
 done
 
@@ -80,17 +99,31 @@ rw [Nat.add_assoc]
 rw [Nat.add_assoc]
 rw [Nat.add_sub_self_left]
 rw [← Nat.add_assoc]
---rw [← Nat.add_sub]
---rw [← Nat.dvd_add_iff_left hd]
+rw [Nat.add_sub_assoc]
+rw [← Nat.dvd_add_iff_left hd]
+rw [← add_mul]
+rw [pow_two]
+rw [← mul_one_add]
+sorry
+rw [pow_three]
 sorry
 done
 
 --exercise 3.3
-example (k : ℕ) : (x + y) ^ n = ∑ k in range (n + 1), x^(n - k) * y^k * Nat.choose k n := by
+example (k : ℕ) : (x + y) ^ n = ∑ k in range (n + 1), x^(n - k) * y^k * choose k n := by
 induction' n with d hd
 rw [_root_.pow_zero]
 rw [Nat.zero_add]
+rw [sum_range_one]
 rw [Nat.choose_zero_right]
-sorry
+rw [_root_.pow_zero]
+rw [_root_.pow_zero]
+simp
+rw [succ_eq_add_one]
+rw [pow_add]
+rw [hd]
+rw [pow_one]
+rw [mul_add]
+
 sorry
 done
